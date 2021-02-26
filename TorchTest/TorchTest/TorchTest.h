ef5f6ad08b53336b06721ea10a8b1b48f7915e4a
Aymen Letaief <letaief.aymen@gmail.com>
2021-02-26 16:51:16 +0100

    Added TorchTest xcode project

---
+++ TorchTest/TorchTest/TorchTest.h
@@ -0,0 +1,18 @@
+//
+//  TorchTest.h
+//  TorchTest
+//
+//  Created by Aymen Letaief on 26.02.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TorchTest.
+FOUNDATION_EXPORT double TorchTestVersionNumber;
+
+//! Project version string for TorchTest.
+FOUNDATION_EXPORT const unsigned char TorchTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TorchTest/PublicHeader.h>
+
+
